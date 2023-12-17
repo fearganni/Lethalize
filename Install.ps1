5b7067e1253995e3462b9e63e686845fd5ce6e42
Gannicus <gannicus@orbitus.ca>
2023-12-17 03:22:47 -0500

    Update everything, add a progress for downloads, and make the code streamlined

--- Install.ps1
+++ Install.ps1
@@ -1,6 +1,7 @@
+# Utility Functions
+
 function Get-PlatformInfo {
     $arch = [System.Environment]::GetEnvironmentVariable("PROCESSOR_ARCHITECTURE")
-    
     switch ($arch) {
         "AMD64" { return "X64" }
         "IA64" { return "X64" }
@@ -11,64 +12,61 @@ function Get-PlatformInfo {
     }
 }
 
-function Request-String($url) {
-    $webClient = New-Object System.Net.WebClient
-    $webClient.Headers.Add("User-Agent", "Lethal Mod Installer PowerShell Script")
-    return $webClient.DownloadString($url)
-}
-
 function Request-Stream($url) {
-    $webClient = New-Object System.Net.WebClient
-    $webClient.Headers.Add("User-Agent", "Lethal Mod Installer PowerShell Script")
-    return [System.IO.MemoryStream]::new($webClient.DownloadData($url))
+    $httpClient = New-Object System.Net.Http.HttpClient
+    $httpClient.DefaultRequestHeaders.Add("User-Agent", "Lethal Mod Installer PowerShell Script")
+
+    $response = $httpClient.GetAsync($url, [System.Net.Http.HttpCompletionOption]::ResponseHeadersRead).Result
+    $contentStream = $response.Content.ReadAsStreamAsync().Result
+
+    $totalBytes = $response.Content.Headers.ContentLength
+    $totalRead = 0
+    $buffer = New-Object byte[](1024 * 1024)  # 1MB buffer
+    $progressStream = New-Object System.IO.MemoryStream
+
+    while (($read = $contentStream.Read($buffer, 0, $buffer.Length)) -gt 0) {
+        $progressStream.Write($buffer, 0, $read)
+        $totalRead += $read
+        $percentComplete = ($totalRead / $totalBytes) * 100
+        Write-Progress -Activity "Downloading" -Status "$percentComplete% Complete" -PercentComplete $percentComplete
+    }
+
+    $progressStream.Seek(0, [System.IO.SeekOrigin]::Begin)
+    return $progressStream
 }
 
 function Expand-Stream($zipStream, $destination) {
-    # Create a temporary file to save the stream content
     $tempFilePath = [System.IO.Path]::GetTempFileName()
-
-    # replace the temporary file extension with .zip
     $tempFilePath = [System.IO.Path]::ChangeExtension($tempFilePath, "zip")
-
-    # Save the stream content to the temporary file
     $zipStream.Seek(0, [System.IO.SeekOrigin]::Begin)
     $fileStream = [System.IO.File]::OpenWrite($tempFilePath)
     $zipStream.CopyTo($fileStream)
     $fileStream.Close()
-
-    # extract the temporary file to the destination folder
     Expand-Archive -Path $tempFilePath -DestinationPath $destination -Force
-
-    # Delete the temporary file
     Remove-Item -Path $tempFilePath -Force
 }
 
 function Get-Arg($arguments, $argName) {
     $argIndex = [Array]::IndexOf($arguments, $argName)
     if ($argIndex -eq -1) {
-        # report error
         throw "Argument $argName not found"
     }
     return $arguments[$argIndex + 1]
 }
 
+# Main Installation Function
+
 function Install ($arguments) {
-    Write-Host "Thanks to Daddy Ganni 🙏"
-    Write-Host ""
+    Write-Host "Please send thanks to Daddy Ganni 🙏`n"
 
-    $response = Request-String "https://api.github.com/repos/BepInEx/BepInEx/releases/latest"
+    $response = Request-Stream "https://api.github.com/repos/BepInEx/BepInEx/releases/latest"
     $jsonObject = ConvertFrom-Json $response
-
     $platform2Asset = @{}
 
-    foreach ($assetNode in $jsonObject.assets) {
-        if ($null -eq $assetNode) { continue }
-        
-        $asset = $assetNode
+    foreach ($asset in $jsonObject.assets) {
+        if ($null -eq $asset) { continue }
 
-        $name = $asset.name
-
-        switch -Wildcard ($name) {
+        switch -Wildcard ($asset.name) {
             "BepInEx_unix*" { $platform2Asset["Unix"] = $asset.browser_download_url; break }
             "BepInEx_x64*" { $platform2Asset["X64"] = $asset.browser_download_url; break }
             "BepInEx_x86*" { $platform2Asset["X86"] = $asset.browser_download_url; break }
@@ -79,17 +77,13 @@ function Install ($arguments) {
     Write-Host "Detected platform: $platform"
 
     $assetUrl = $platform2Asset[$platform]
-
     if ($null -eq $assetUrl) {
         throw "Failed to find asset for platform $platform"
     }
 
     Write-Host "Downloading $assetUrl"
-
     $stream = Request-Stream $assetUrl
-    
-    Write-Host "Downloaded $assetUrl"
-    Write-Host ""
+    Write-Host "Downloaded $assetUrl`n"
 
     $lethalCompanyPath = (Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 1966720").InstallLocation
     if ($null -eq $lethalCompanyPath) {
@@ -97,50 +91,33 @@ function Install ($arguments) {
     }
     
     $bepInExPath = Join-Path $lethalCompanyPath "BepInEx"
+    Write-Host "Lethal Company path: $lethalCompanyPath`n"
 
-    Write-Host "Lethal Company path: $lethalCompanyPath"
-    Write-Host ""
-
-    # Delete old files
     if (Test-Path $bepInExPath) {
         Write-Host "Deleting old files"
         Remove-Item $bepInExPath -Recurse -Force
-        Write-Host "Deleted old files"
-        Write-Host ""
+        Write-Host "Deleted old files`n"
     }
 
     Write-Host "Installing BepInEx"
     Expand-Stream $stream $lethalCompanyPath
-    Write-Host "Installed BepInEx"
-    Write-Host ""
+    Write-Host "Installed BepInEx`n"
 
-    # Download and install lcapi
-    Write-Host "Downloading and installing LC_API"
-    $lcapiVersion = Get-Arg $arguments "-lcapi"
-    $lcapiUrl = "https://thunderstore.io/package/download/2018/LC_API/$lcapiVersion/"
-    $lcapiStream = Request-Stream $lcapiUrl
-    Expand-Stream $lcapiStream $lethalCompanyPath
-    Write-Host "Installed LC_API"
-    Write-Host ""
-
-    # Download and install biggerlobby
-    Write-Host "Downloading and installing BiggerLobby"
-    $biggerLobbyVersion = Get-Arg $arguments "-biggerlobby"
-    $biggerLobbyUrl = "https://thunderstore.io/package/download/bizzlemip/BiggerLobby/$biggerLobbyVersion/"
-    $biggerLobbyStream = Request-Stream $biggerLobbyUrl
-    Expand-Stream $biggerLobbyStream $lethalCompanyPath
-    Write-Host "Installed BiggerLobby"
-    Write-Host ""
-
-    # Download and install biggerlobby
-    Write-Host "Downloading and installing MoreSuits"
-    $moreSuitsVersion = Get-Arg $arguments "-moresuits"
-    $moreSuitsUrl = "https://thunderstore.io/package/download/x753/More_Suits/$moreSuitsVersion/"
-    $moreSuitsStream = Request-Stream $moreSuitsUrl
-    Expand-Stream $moreSuitsStream $lethalCompanyPath
-    Write-Host "Installed MoreSuits"
-    Write-Host ""
+    # Can add your own arguments here
+    Install-Mod "2018" "LC_API" (Get-Arg $arguments "-lcapi") $lethalCompanyPath
+    Install-Mod "bizzlemip" "BiggerLobby" (Get-Arg $arguments "-biggerlobby") $lethalCompanyPath
+    Install-Mod "x753" "More_Suits" (Get-Arg $arguments "-moresuits") $lethalCompanyPath
 }
+
+function Install-Mod($modAuthor, $modName, $version, $path) {
+    Write-Host "Downloading and installing $modName by $modAuthor"
+    $url = "https://thunderstore.io/package/download/$modAuthor/$modName/$version/"
+    $stream = Request-Stream $url
+    Expand-Stream $stream $path
+    Write-Host "Installed $modName`n"
+}
+
+# Script Execution
 
 try {
     Install $args
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Brr
 
 ```powershell
-powershell -nop -ExecutionPolicy Bypass -c "Invoke-Command -ScriptBlock ([scriptblock]::Create([System.Text.Encoding]::UTF8.GetString((New-Object Net.WebClient).DownloadData('https://github.com/fearganni/Lethalize/releases/download/0.0.1/Install.ps1')))) -ArgumentList @('-lcapi','2.2.0','-biggerlobby','2.6.0','-moresuits','1.4.1')"
+powershell -nop -ExecutionPolicy Bypass -c "Invoke-Command -ScriptBlock ([scriptblock]::Create([System.Text.Encoding]::UTF8.GetString((New-Object Net.WebClient).DownloadData('https://github.com/fearganni/Lethalize/releases/latest/download/Install.ps1')))) -ArgumentList @('-lcapi','2.2.0','-biggerlobby','2.6.0','-moresuits','1.4.1')"
 ```
