b11f1eb610377c18af30074c3d9f4118b2204798
Feargannicus <gannicus@exordium.org>
2024-01-17 22:58:47 -0500

    Change the location for the diversity plugin

--- Install.ps1
+++ Install.ps1
@@ -105,7 +105,7 @@ function Install ($arguments) {
     Install-Mod "notnotnotswipez" "MoreCompany" (Get-Arg $arguments "-moreCompany") $lethalCompanyPath
     Install-Mod "x753" "More_Suits" (Get-Arg $arguments "-moreSuits") $lethalCompanyPath
     Install-Mod "Sligili" "More_Emotes" (Get-Arg $arguments "-moreEmotes") $lethalCompanyPath
-    Install-Mod "IntegrityChaos" "Diversity" (Get-Arg $arguments "-diversity") $lethalCompanyPath
+    Install-Mod "IntegrityChaos" "Diversity" (Get-Arg $arguments "-diversity") $bepInExPath
 }
 
 function Install-Mod($modAuthor, $modName, $version, $path) {
